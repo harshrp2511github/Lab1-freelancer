78df898f338e63df3da99f1b8b5e84743845689d
harshrp2511github <harshrp2511@gmail.com>
2018-03-18 02:45:19 -0700

    Added Mocha tests

--- server/test/test.js
+++ server/test/test.js
@@ -3,9 +3,9 @@ var request = require('request');
 var http = require("http");
 var express = require('express')
 
-describe('Positive SignIn Test', function() {
+describe('Positive Login Test', function() {
 
-    it('should signIn with correct username and password as parameters', function (done) {
+    it('user should login with correct username and password', function (done) {
         request.post('http://localhost:3001/users/login', {
             form: {
                 email: 'root@root.com',
@@ -13,11 +13,152 @@ describe('Positive SignIn Test', function() {
                 credentials: true
             }
         }, function (error, response, body) {
-            console.log(response);
+            console.log(response.statusCode);
             assert.equal(201, response.statusCode);
             done();
         });
     });
-
-
 });
+
+describe('Negative Login Test', function() {
+
+    it('user should not be able to login with incorrect username and password', function (done) {
+        request.post('http://localhost:3001/users/login', {
+            form: {
+                email: 'root@root.com',
+                password: 'root',
+                credentials: true
+            }
+        }, function (error, response, body) {
+            console.log(response.statusCode);
+            assert.equal(401, response.statusCode);
+            done();
+        });
+    });
+});
+
+// describe('Positive Signup Test', function() {
+//
+//     it('user should  be able to signup with unique email, and valid username and password', function (done) {
+//         request.post('http://localhost:3001/users/signup', {
+//             form: {
+//                 email: 'root3@root3.com',
+//                 username: 'root1',
+//                 password: 'root1.12345',
+//                 credentials: true
+//             }
+//         }, function (error, response, body) {
+//             console.log(response.statusCode);
+//             assert.equal(201, response.statusCode);
+//             done();
+//         });
+//     });
+// });
+
+describe('Negative Signup Test', function() {
+
+    it('user should  not be able to signup with Existing email, or invalid (username and password)', function (done) {
+        request.post('http://localhost:3001/users/signup', {
+            form: {
+                email: 'root@root.com',
+                username: 'root1',
+                password: 'root1.12345',
+                credentials: true
+            }
+        }, function (error, response, body) {
+            console.log(response.statusCode);
+            assert.equal(401, response.statusCode);
+            done();
+        });
+    });
+});
+
+describe('Positive Placebid Test', function() {
+
+    it('user should be able to bid on a project he has not already bid on', function (done) {
+        request.post('http://localhost:3001/users/addbid', {
+            form: {
+                projectname:'Website2',
+                biddingparty:'rt@rt.com',
+                name: 'Rachel',
+                price: 300,
+                days: 7,
+                credentials: true
+            }
+        }, function (error, response, body) {
+            console.log(response.statusCode);
+            assert.equal(201, response.statusCode);
+            done();
+        });
+    });
+});
+
+describe('Negative Placebid Test', function() {
+
+    it('user should not be able to bid on a project he has  already bid on', function (done) {
+        request.post('http://localhost:3001/users/addbid', {
+            form: {
+                projectname:'Website2',
+                biddingparty:'rt@rt.com',
+                name: 'Rachel',
+                price: 300,
+                days: 7,
+                credentials: true
+            }
+        }, function (error, response, body) {
+            console.log(response.statusCode);
+            assert.equal(401, response.statusCode);
+            done();
+        });
+    });
+});
+
+describe('Positive get My Posted Projects  Test', function() {
+
+    it('user should be able to get list of all projects he has posted', function (done) {
+        request.post('http://localhost:3001/users/getmyprojectlist', {
+            form: {
+                email: 'root@root.com',
+                credentials: true
+            }
+        }, function (error, response, body) {
+            console.log(response.statusCode);
+            assert.equal(201, response.statusCode);
+            done();
+        });
+    });
+});
+
+describe('Positive get My Posted Projects  Test', function() {
+
+    it('user should be able to get list of all projects he has posted', function (done) {
+        request.post('http://localhost:3001/users/getmyprojectlist', {
+            form: {
+                email: 'root@root.com',
+                credentials: true
+            }
+        }, function (error, response, body) {
+            console.log(response.statusCode);
+            assert.equal(201, response.statusCode);
+            done();
+        });
+    });
+});
+
+describe('Positive get Bids on Projects  Test', function() {
+
+    it('user should be able to get list of all bids on the project', function (done) {
+        request.post('http://localhost:3001/users/getbids', {
+            form: {
+                projectname: 'Website2',
+                credentials: true
+            }
+        }, function (error, response, body) {
+            console.log(response.statusCode);
+            assert.equal(201, response.statusCode);
+            done();
+        });
+    });
+});
+
+
